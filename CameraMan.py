b88903a7028d05803b51408009942a0342a6242f
Aidon Lebar <aidonlebar@gmail.com>
2018-05-29 14:44:54 -0400

    USB auto-unmounts at end of program.

--- CameraMan.py
+++ CameraMan.py
@@ -3,7 +3,8 @@ import time
 from time import gmtime, strftime
 from datetime import datetime
 import shutil
-from os import listdir
+import os
+import re
 
 #record and preview
 cmd = 'ffmpeg -rtbufsize 256MB -video_size 1280x720 -pix_fmt uyvy422 -framerate 30 -f avfoundation -i "0:0" -vf hflip -c:v rawvideo -f nut -map 0:v pipe: \
@@ -13,14 +14,26 @@ cmd = 'ffmpeg -rtbufsize 256MB -video_size 1280x720 -pix_fmt uyvy422 -framerate 
 filename = "{}.avi".format(datetime.now().strftime('%Y-%m-%d %Hh%M'))
 cmd2 = 'ffmpeg -i output.avi -vf hflip -c:v libx264 -crf 17 -f avi \"{}\"'.format(filename)
 
-devices = listdir('/Volumes')
+devices = os.listdir('/Volumes')
+
+d = os.listdir('/dev')
+r = re.compile('^disk.$')
+d = list(filter(r.match, d))
+
 print("Please Insert USB Storage Device.")
 
 while(True):
-    devices2 = listdir('/Volumes')
+    devices2 = os.listdir('/Volumes')
     l = [x for x in devices2 if x not in devices]
+
     if l:
         usb = l[0]
+
+        d2 = os.listdir('/dev')
+        d2 = list(filter(r.match, d2))
+        d2 = [x for x in d2 if x not in d]
+        d2 = d2[0]
+
         break
 print("Found USB: {}".format(usb))
 
@@ -43,8 +56,11 @@ print("Recording Completed. Compressing...")
 p2 = subprocess.Popen(cmd2, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 p2.wait()
 p3 = subprocess.Popen('rm output.avi', stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-print("Compression Complete. Moving video to USB...")
+print("Compression Complete. Moving Video to USB...")
 
 shutil.move("./{}".format(filename), "/Volumes/{}/{}".format(usb, filename))
 
-print("Move Completed. Please Eject and Remove USB Storage Device")
+p4 = subprocess.Popen('diskutil unmountDisk /dev/{}'.format(d2), stdout=subprocess.PIPE, shell=True)
+p4.wait()
+
+print("Completed. Please Remove USB Storage Device")
