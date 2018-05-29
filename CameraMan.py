5ab0559a2258066a9d74734dd9b2d3a1294d8a53
Aidon Lebar <aidonlebar@gmail.com>
2018-05-29 10:29:23 -0400

    Preview and save both in HD.

--- CameraMan.py
+++ CameraMan.py
@@ -1,11 +1,8 @@
 import subprocess
 import time
 
-#cmd = 'ffmpeg -rtbufsize 702000k -video_size 1280x720 -pix_fmt uyvy422 -framerate 30 -f avfoundation -i "0:0" -c:v libx264 -tune zerolatency -crf 0 -preset ultrafast -f avi -map 0:v pipe: \
-#-f avi output.avi | ffplay pipe:'
-
-cmd = 'ffmpeg -rtbufsize 702000k -video_size 1280x720 -pix_fmt uyvy422 -framerate 30 -f avfoundation -i "0:0" -f avi -map 0:v pipe: \
--f avi output.avi | ffplay pipe:'
+cmd = 'ffmpeg -rtbufsize 256MB -video_size 1280x720 -pix_fmt uyvy422 -framerate 30 -f avfoundation -i "0:0" -c:v rawvideo -f nut -map 0:v pipe: \
+-f avi -c:v copy output.avi | ffplay pipe:'
 
 input("Press Enter to Start Recording.")
 print("Recording")
