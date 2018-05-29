e177ce992b6ecdf5f1a36294febb763ce1df0c41
Aidon Lebar <aidonlebar@gmail.com>
2018-05-28 22:33:36 -0400

    Now records with preview.

--- CameraMan.py
+++ CameraMan.py
@@ -1,14 +1,18 @@
 import subprocess
 import time
 
-cmd = 'ffmpeg -pix_fmt uyvy422 -f avfoundation -framerate 30 -i \"0:0\" output.avi -hide_banner'
+#cmd = 'ffmpeg -rtbufsize 702000k -video_size 1280x720 -pix_fmt uyvy422 -framerate 30 -f avfoundation -i "0:0" -c:v libx264 -tune zerolatency -crf 0 -preset ultrafast -f avi -map 0:v pipe: \
+#-f avi output.avi | ffplay pipe:'
+
+cmd = 'ffmpeg -rtbufsize 702000k -video_size 1280x720 -pix_fmt uyvy422 -framerate 30 -f avfoundation -i "0:0" -f avi -map 0:v pipe: \
+-f avi output.avi | ffplay pipe:'
 
 input("Press Enter to Start Recording.")
 print("Recording")
 
-p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
-input()
+input("Press Enter to Stop Filming.")
 p.terminate()
 
 print("Recording Completed.")
